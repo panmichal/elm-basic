18cdaaa7f4a331aa0d246714d5ad3b0ca19ccdc3
Michal Strzelczyk <mich.strzelczyk@gmail.com>
2016-03-10 22:54:20 -0500

    Mouse click with mailbox

--- Model.elm
+++ Model.elm
@@ -1,33 +1,54 @@
 module Main (..) where
 
 import Html exposing (Html)
-import Mouse
+import Html.Events as Events
 
 type alias Model =
   { count : Int
   }
+
+type Action
+  = NoOp
+  | MouseClick
+  | KeyPress
 
 initialModel : Model
 initialModel =
   { count = 0
   }
 
-update : () -> Model -> Model
-update _ model =
-  { model | count = model.count + 1
-  }
+update : Action -> Model -> Model
+update action model =
+  case action of
+    MouseClick ->
+  { model | count = model.count + 1 }
+
+    KeyPress ->
+  { model | count = model.count - 1 }
+
+    _ ->
+      model
 
 
-view : Model -> Html
-view model =
-  Html.text (toString model.count)
+view : Signal.Address Action -> Model -> Html
+view address model =
+  Html.div
+    []
+    [ Html.div [] [ Html.text (toString model.count) ]
+    , Html.button
+      [ Events.onClick address MouseClick ]
+      [ Html.text "Increase" ]
+    ]
 
+mb : Signal.Mailbox Action
+mb =
+  Signal.mailbox NoOp
 
 modelSignal : Signal Model
 modelSignal =
-  Signal.foldp update initialModel Mouse.clicks
+  Signal.foldp update initialModel mb.signal
 
 
 main : Signal.Signal Html
 main =
-  Signal.map view modelSignal
+  Signal.map (view mb.address) modelSignal
